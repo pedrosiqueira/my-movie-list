189d78403388ff0307dbd7dfdacb4b9fb7c3e9d6
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2024-01-31 22:36:25 -0400

    apagando as migrations

--- prisma/migrations/20240131122932_m1/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `avaliacaoMetascore` on the `Filme` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Filme" DROP COLUMN "avaliacaoMetascore",
-ADD COLUMN     "avaliacaoMetacritic" INTEGER;
